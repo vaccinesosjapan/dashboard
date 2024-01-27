5af0a8e898514ee3e718d06d46f0f22d8caca90a
kaede96az <147464913+kaede96az@users.noreply.github.com>
2024-01-28 00:22:37 +0900

    fix: 表示する文字数制限の処理不具合を修正

--- src/components/StringArrayRow.vue
+++ src/components/StringArrayRow.vue
@@ -1,10 +1,10 @@
 <template>
 	<div v-if="sArray.length == 1">
-		<span v-if="sArray[0].length >= 10">{{ sArray[0].substring(0, 7) }}</span>
+		<span v-if="sArray[0].length > 10">{{ sArray[0].substring(0, 7) + '...' }}</span>
 		<span v-else>{{ sArray[0] }}</span>
     </div>
 	<div v-else-if="sArray.length > 1">
-		<span v-if="sArray[0].length >= 10">{{ sArray[0].substring(0, 7) + '...' }}</span>
+		<span v-if="sArray[0].length > 10">{{ sArray[0].substring(0, 7) + '...' }}</span>
 		<span v-else>{{ sArray[0] + '...' }}</span>
     </div>
 	<div v-else>-</div>
