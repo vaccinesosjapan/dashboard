ce2f0e784c4e8b751525a6189709d3487465fe92
kaede96az <147464913+kaede96az@users.noreply.github.com>
2024-04-27 09:53:49 +0900

    feat: improve look and feel for top page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -24,15 +24,15 @@ export const SuspectedIssuesSubRoutes = [
   {
     path: '/reported-myocarditis-issues',
     name: '製造販売業者からの報告 - 心筋炎/心膜炎報告',
-    menu_name: '心筋炎/心膜炎報告',
-    icon: 'mdi-account-search',
+    menu_name: '心筋炎/心膜炎報告を検索',
+    icon: 'mdi-magnify',
     component: () => import('../views/MyocarditisView.vue')
   },
   {
     path: '/reported-death-issues',
     name: '製造販売業者からの報告 - 死亡報告',
-    menu_name: '死亡報告',
-    icon: 'mdi-account-search',
+    menu_name: '死亡報告を検索',
+    icon: 'mdi-magnify',
     component: () => import('../views/DeathView.vue')
   },
 ]
@@ -48,8 +48,8 @@ export const MedialInstitutionRoutes = [
   {
     path: '/reports-from-medical-institution',
     name: '医療機関からの報告 - 副反応疑い報告',
-    menu_name: '副反応疑い報告',
-    icon: 'mdi-account-search',
+    menu_name: '副反応疑い報告を検索',
+    icon: 'mdi-magnify',
     component: () => import('../views/MedicalInstitutionView.vue')
   },
 ]
@@ -66,15 +66,15 @@ export const HealthHazardsSubRoutes = [
   {
     path: '/certified-issues',
     name: '予防接種健康被害 救済制度 - 判定済み 報告一覧',
-    menu_name: '判定済み 報告一覧',
-    icon: 'mdi-account-search',
+    menu_name: '判定済み一覧を検索',
+    icon: 'mdi-magnify',
     component: () => import('../views/CertifiedHealthHazardsView.vue')
   },
   {
     path: '/certified-symptoms',
     name: '予防接種健康被害 救済制度 - 認定済み 症状一覧',
-    menu_name: '認定済み 症状一覧',
-    icon: 'mdi-account-search',
+    menu_name: '認定済み症状を検索',
+    icon: 'mdi-magnify',
     component: () => import('../views/CertifiedSymptomsView.vue')
   }
 ]
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,7 +9,15 @@ AppBarColor.value = 'blue-grey-lighten-4'
 <template>
   <v-container>
     <v-sheet>
-      <h2 class="text-h4">データについて</h2>
+      <h2 class="text-h4">本サイトについて</h2>
+      <p class="text-body-1">
+        厚生労働省のサイトには、新型コロナワクチンの「<a href="https://www.mhlw.go.jp/stf/shingi/shingi-kousei_284075.html">副反応疑い報告</a>」や「<a href="https://www.mhlw.go.jp/stf/shingi/shingi-shippei_127696_00001.html">予防接種健康被害</a>」に関する情報が
+        PDF形式で多数掲載されています。それらのデータを見やすく整理して検索可能にし、より多くの人にとって
+        見やすく、知見を得やすい情報源にするためのサイトです。
+      </p>
+      <br>
+
+      <h3 class="text-h5">データについて</h3>
       <p class="text-body-1">
         「厚生科学審議会予防接種・ワクチン分科会副反応検討部会」のPDF資料から抽出したデータを使っています。
         できるだけ正確に情報を抽出するよう努めておりますが、当方はデータの正確さについて一切の責任を負いません。
