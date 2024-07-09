ad7a2626783a74b068092b8e494b53ea59bbdecf
kaede96az <147464913+kaede96az@users.noreply.github.com>
2024-07-09 23:50:32 +0900

    feat: 目次機能を追加し、使い方ページの内容を充実させた

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,13 @@ import {
   HomeRoute,
   HealthHazardsSubRoutes
 } from '@/router/routes'
-import { AppBarTitle, AppBarColor } from '@/router/data'
+import { AppBarTitle, AppBarColor, AppBarUseTableOfContent, AppBarTableOfContentFunc } from '@/router/data'
 import { shallowRef } from 'vue'
 
-const selectedItem = shallowRef('')
-
 const baseURL = import.meta.env.BASE_URL
-</script>
 
-<script lang="ts">
-export default {
-  data: () => ({
-    drawer: false
-  })
-}
+const selectedItem = shallowRef('')
+const drawer = shallowRef<boolean>(false)
 </script>
 
 <template>
@@ -36,6 +29,10 @@ export default {
         <v-toolbar-title class="d-none d-sm-flex">{{ AppBarTitle }}</v-toolbar-title>
         <v-toolbar-title class="d-flex d-sm-none small-app-title">{{ AppBarTitle }}</v-toolbar-title>
       </template>
+      <v-spacer v-if="AppBarUseTableOfContent"></v-spacer>
+      <v-btn v-if="AppBarUseTableOfContent" prepend-icon="mdi-table-of-contents" @click.stop="AppBarTableOfContentFunc">
+        目次
+      </v-btn>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" temporary width="10rem">
--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
   scrollBehavior(to) {
     const param = to.query['anchor']
     if (param) {
+      // why: https://v3.router.vuejs.org/ja/guide/advanced/scroll-behavior.html#%E9%9D%9E%E5%90%8C%E6%9C%9F%E3%81%AA%E3%82%B9%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%99
+      // 少し待ちを入れないと画像の描画が後になって、思ったような位置にスクロールできないため。
+      // vueの仕組みで「画像も含めて全部読み込み完了」したタイミングがイベントとれるなら、
+      // こんな固定時間の待ちをいれるなどという「環境によっては間に合わない or 待ちすぎ」
+      // みたいなコードを入れる必要もなくなるはず。
       return new Promise(() => {
         setTimeout(() => {
           const chapterElement = document.getElementById(param.toString())
